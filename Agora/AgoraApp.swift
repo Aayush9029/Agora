ae3d6011894de93a048de190969bf47e30e3369b
Aayush9029 <developer@aayush.art>
2023-09-29 18:24:43 -0400

    Initial Commit

---
+++ Agora/AgoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgoraApp.swift
+//  Agora
+//
+//  Created by Aayush Pokharel on 2023-09-29.
+//
+
+import SwiftUI
+
+@main
+struct AgoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Agora/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Agora
+//
+//  Created by Aayush Pokharel on 2023-09-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
